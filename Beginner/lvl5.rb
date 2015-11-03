61b36e90894d79a35c6e37b05f73f20737d1161c
Daphne <dg1181@nyu.edu>
2015-11-02 19:45:44 -0800

    lvl 5 complete

---
+++ Beginner/lvl5.rb
@@ -0,0 +1,18 @@
+class Player
+  def play_turn(warrior)
+    if warrior.feel.empty?
+      if warrior.health == 20 || warrior.health < @health
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    else
+      warrior.attack!
+    end
+
+    @health = warrior.health
+  end
+end
+  
