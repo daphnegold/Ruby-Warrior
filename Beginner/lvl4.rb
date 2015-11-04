317cf71d61d6ac92939bfebf0a5dc3c6ad0ca312
Daphne <dg1181@nyu.edu>
2015-11-03 23:18:37 -0800

    lvl 4 refactor

--- Beginner/lvl4.rb
+++ Beginner/lvl4.rb
@@ -1,15 +1,27 @@
 class Player
+  HEALTHY = 15
+
   def play_turn(warrior)
+    @health ||= warrior.health
+
     if warrior.feel.empty?
-      if warrior.health == 20 || warrior.health < @health
-        warrior.walk!
-      else
+      if rest_time?(warrior)
         warrior.rest!
+      else
+        warrior.walk!
       end
     else
       warrior.attack!
     end
 
     @health = warrior.health
+  end
+
+  def under_attack?(warrior)
+    warrior.health < @health
+  end
+
+  def rest_time?(warrior)
+    warrior.health < HEALTHY && !under_attack?(warrior)
   end
 end
