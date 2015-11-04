b27d27520f8849009f616f05a011446faf353531
Daphne <dg1181@nyu.edu>
2015-11-03 23:19:17 -0800

    lvl 8 refactor

--- Beginner/lvl8.rb
+++ Beginner/lvl8.rb
@@ -1,29 +1,62 @@
 class Player
+  HEALTHY = 15
+  FLEE = 8
+
   def play_turn(warrior)
+    @health ||= warrior.health
     @direction ||= :forward
+    @fled ||= false
+    @found_wall ||= false
 
-    next_thing = warrior.look(@direction).find { |i| i.enemy? || i.captive? }
-
-    if warrior.feel(@direction).wall?
-      warrior.pivot!
-    elsif !next_thing.nil? && next_thing.enemy?
-      warrior.shoot!
-    elsif warrior.feel(@direction).empty?
-      if warrior.health == 20 || (warrior.health < @health && warrior.health > 10)
-        warrior.walk!(@direction)
-      elsif warrior.health < @health
-        warrior.walk!(:backward)
-      else
+    if warrior.feel(@direction).empty?
+      if shoot?(warrior)
+        warrior.shoot!
+      elsif rest_time?(warrior)
         warrior.rest!
+        switch_direction(warrior) if @fled && @found_wall
+        @fled = false
+      elsif flee?(warrior)
+        switch_direction(warrior) unless @fled
+        @fled = true
+        warrior.walk!(@direction)
+      else
+        warrior.walk!(@direction)
       end
     elsif warrior.feel(@direction).captive?
       warrior.rescue!(@direction)
-    elsif warrior.health > 10
-      warrior.attack!(@direction)
+    elsif warrior.feel(@direction).wall?
+      warrior.pivot!
+      @found_wall = true
     else
-      warrior.walk!(:backward)
+      warrior.attack!
     end
 
     @health = warrior.health
   end
+
+  def under_attack?(warrior)
+    warrior.health < @health
+  end
+
+  def rest_time?(warrior)
+    warrior.health < HEALTHY && !under_attack?(warrior)
+  end
+
+  def switch_direction(warrior)
+    case @direction
+    when :forward then @direction = :backward
+    when :backward then @direction = :forward
+    end
+  end
+
+  def flee?(warrior)
+    warrior.health < FLEE
+  end
+
+  def shoot?(warrior)
+    @next_thing = warrior.look(@direction).find { |i| i.enemy? || i.captive? }
+
+    @next_thing.enemy? if !@next_thing.nil?
+  end
+
 end
